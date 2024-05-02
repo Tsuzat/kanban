6e29f774f4070cb730fb715b892eb47598e2044b
Tsuzat <dev.tsuzat@gmail.com>
2024-05-02 23:07:53 +0530

    add: main layout and page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,18 @@
 <script lang="ts">
+	import { ModeWatcher } from 'mode-watcher';
+	import '@fontsource-variable/dm-sans';
 	import '../app.pcss';
+	import Navbar from '$lib/components/Navbar.svelte';
+	import { Toaster } from '$lib/components/ui/sonner';
 </script>
 
-<slot></slot>
+<svelte:head>
+	<title>Kanban Board</title>
+</svelte:head>
+
+<ModeWatcher />
+<Toaster />
+<div class="flex h-screen w-full">
+	<Navbar />
+	<slot />
+</div>
