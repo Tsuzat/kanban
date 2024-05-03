98b2e957873f1e8ca4c016a619c8f3559468a97b
Tsuzat <dev.tsuzat@gmail.com>
2024-05-03 23:30:33 +0530

    improved layouts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,8 @@
 	import '../app.pcss';
 	import Navbar from '$lib/components/Navbar.svelte';
 	import { Toaster } from '$lib/components/ui/sonner';
+	import { Menu } from '$lib/icons';
+	import { Button } from '$lib/components/ui/button';
 </script>
 
 <svelte:head>
@@ -12,7 +14,7 @@
 
 <ModeWatcher />
 <Toaster />
-<div class="flex h-screen w-full">
+<div class="flex h-screen w-screen">
 	<Navbar />
 	<slot />
 </div>
