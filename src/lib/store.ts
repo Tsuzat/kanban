dd6b80fe7114410dcc26a5c960679232ee09eaca
Tsuzat <dev.tsuzat@gmail.com>
2024-05-03 23:25:45 +0530

    improved: store

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,29 +1,40 @@
 import { writable, type Writable } from 'svelte/store';
 import KanbanBoard from './types/KanbanBoard';
 import { browser } from '$app/environment';
+import { toast } from 'svelte-sonner';
+import type { KanbanBoardType } from './types/types';
 
 export const KanBans: Writable<KanbanBoard[]> = writable([]);
 
-KanBans.subscribe((value) => {
-	console.log('Updated KanBans');
-	console.log(value);
-});
+// KanBans.subscribe((value) => {
+// 	console.log('Updated KanBans');
+// 	console.log(value);
+// });
 
 if (browser) {
-	const allItems = [];
 	const prefix = 'kanban-board-';
+	const kanbans: KanbanBoard[] = [];
 
 	for (const key in localStorage) {
 		if (key.startsWith(prefix)) {
 			const value = localStorage.getItem(key);
 			if (value) {
-				allItems.push(JSON.parse(value));
+				try {
+					const tmpKanBanJSON: KanbanBoardType = JSON.parse(value);
+					const tmpKanBan: KanbanBoard = KanbanBoard.fromJSON(tmpKanBanJSON);
+					kanbans.push(tmpKanBan);
+				} catch (error) {
+					toast.error('Error while parsing data', {
+						description:
+							'Kanban can not be parsed because of corrupted data. This will be automatically deleted ',
+						action: {
+							label: 'Ok',
+							onClick: () => {}
+						}
+					});
+				}
 			}
 		}
-	}
-	let kanbans: KanbanBoard[] = [];
-	for (let k of allItems) {
-		kanbans.push(KanbanBoard.fromJSON(k));
 	}
 	KanBans.set(kanbans);
 }
