dcc818e73c2a99e22c493a337e12adb324fe0bd0
Tsuzat <dev.tsuzat@gmail.com>
2024-05-24 22:29:31 +0530

    add: navbar collapsable to localstore

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -5,36 +5,43 @@ import { toast } from 'svelte-sonner';
 import type { KanbanBoardType } from './types/types';
 
 export const KanBans: Writable<KanbanBoard[]> = writable([]);
-
-// KanBans.subscribe((value) => {
-// 	console.log('Updated KanBans');
-// 	console.log(value);
-// });
+export const isNavbarCollapsed: Writable<Boolean> = writable(false);
 
 if (browser) {
-	const prefix = 'kanban-board-';
-	const kanbans: KanbanBoard[] = [];
+  const prefix = 'kanban-board-';
+  const kanbans: KanbanBoard[] = [];
 
-	for (const key in localStorage) {
-		if (key.startsWith(prefix)) {
-			const value = localStorage.getItem(key);
-			if (value) {
-				try {
-					const tmpKanBanJSON: KanbanBoardType = JSON.parse(value);
-					const tmpKanBan: KanbanBoard = KanbanBoard.fromJSON(tmpKanBanJSON);
-					kanbans.push(tmpKanBan);
-				} catch (error) {
-					toast.error('Error while parsing data', {
-						description:
-							'Kanban can not be parsed because of corrupted data. This will be automatically deleted ',
-						action: {
-							label: 'Ok',
-							onClick: () => {}
-						}
-					});
-				}
-			}
-		}
-	}
-	KanBans.set(kanbans);
+  for (const key in localStorage) {
+    if (key.startsWith(prefix)) {
+      const value = localStorage.getItem(key);
+      if (value) {
+        try {
+          const tmpKanBanJSON: KanbanBoardType = JSON.parse(value);
+          const tmpKanBan: KanbanBoard = KanbanBoard.fromJSON(tmpKanBanJSON);
+          kanbans.push(tmpKanBan);
+        } catch (error) {
+          toast.error('Error while parsing data', {
+            description:
+              'Kanban can not be parsed because of corrupted data. This will be automatically deleted ',
+            action: {
+              label: 'Ok',
+              onClick: () => { }
+            }
+          });
+        }
+      }
+    }
+  }
+  KanBans.set(kanbans);
+
+  let isNavBarCollapsed = localStorage.getItem("kanban-nav-isCollapsed") ?? "false";
+
+  isNavbarCollapsed.set(isNavBarCollapsed === "true");
 }
+
+isNavbarCollapsed.subscribe((value) => {
+  if (browser) {
+    localStorage.setItem("kanban-nav-isCollapsed", value ? "true" : "false");
+  }
+})
+
