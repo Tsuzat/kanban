4df991a4bccfb1f2cb7a749a9b7517dd5f650eb3
Tsuzat <dev.tsuzat@gmail.com>
2024-05-27 13:08:36 +0530

    add: Shadcn/svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import '@fontsource-variable/dm-sans';
+	import '../app.css';
+</script>
+
+<svelte:head>
+	<!-- Primary Meta Tags -->
+	<title>Kanban Board App - Organize Your Tasks Efficiently</title>
+
+</svelte:head>
+
+<slot />
