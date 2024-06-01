7e81eba8451c67e34180ef9fa16f99f33c6f3609
Tsuzat <dev.tsuzat@gmail.com>
2024-06-01 17:37:32 +0530

    add: stores with subscribe and localStore data fetching

---
+++ src/lib/store.ts
@@ -0,0 +1,37 @@
+import { browser } from '$app/environment';
+import { writable } from 'svelte/store';
+import { type Kanban } from './localStore/types';
+import { kanbanFromJSON } from './localStore/utils';
+
+export const isNavbarCollapsed = writable(false);
+
+export const KANBANS = writable<Kanban[]>([]);
+
+if (browser) {
+	// get the navbar collapsed
+	const tmpCollapsed = localStorage.getItem('kanban-navbar-collapsed') ?? 'false';
+	isNavbarCollapsed.set(tmpCollapsed === 'true');
+
+	// Get all the local storage kanbans, the id needs to be started with 'kanban-board-'
+	const localKanbans: Kanban[] = [];
+	for (const key in localStorage) {
+		if (key.startsWith('kanban-board-')) {
+			const rawKanban = localStorage.getItem(key);
+			if (rawKanban) {
+				try {
+					const newKanban = kanbanFromJSON(rawKanban);
+					localKanbans.push(newKanban);
+				} catch (e) {
+					console.error('Error loading kanban from local storage', e);
+					continue;
+				}
+			}
+		}
+	}
+	KANBANS.set(localKanbans);
+}
+
+isNavbarCollapsed.subscribe((value) => {
+	console.log('isNavbarCollapsed', value);
+	if (browser) localStorage.setItem('kanban-navbar-collapsed', value ? 'true' : 'false');
+});
