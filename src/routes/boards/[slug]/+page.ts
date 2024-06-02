7835578b6cfcd2b04620a5d12d8e56dd61296b83
Tsuzat <dev.tsuzat@gmail.com>
2024-06-02 21:56:12 +0530

    fix: invalid kanban redirects to /boards

--- src/routes/boards/[slug]/+page.ts
+++ src/routes/boards/[slug]/+page.ts
@@ -13,17 +13,23 @@ export async function load({ params }) {
 	const kanban: Writable<Kanban> = writable<Kanban>();
 
 	const rawKanban = localStorage.getItem(`kanban-board-${params.slug}`);
-	if (rawKanban) {
-		try {
-			tmpKanban = kanbanFromJSON(rawKanban);
-			if (tmpKanban !== null) {
-				kanban.set(tmpKanban);
-			}
-		} catch (e) {
-			console.error('Error loading kanban from local storage', e);
-			toast.error('Error loading kanban from local storage');
-			redirect(404, '/boards');
-		}
+
+	if (rawKanban === null) {
+		alert(
+			`Kanban with id = ${params.slug} can not be found in local storage. You'll be redirected to /boards`
+		);
+		redirect(308, '/boards');
+	}
+
+	try {
+		tmpKanban = kanbanFromJSON(rawKanban);
+		kanban.set(tmpKanban);
+	} catch (e) {
+		console.error('Error loading kanban from local storage', e);
+		toast.error('Error loading kanban from local storage', {
+			description: 'Looks like kanban board does not exist in your local storage.'
+		});
+		redirect(308, '/boards');
 	}
 
 	return {
