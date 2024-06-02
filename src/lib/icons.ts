9b3928f9903a96fab9329fb49edffe50e18c2a98
Tsuzat <dev.tsuzat@gmail.com>
2024-06-02 17:38:24 +0530

    update: moon icon

--- src/lib/icons.ts
+++ src/lib/icons.ts
@@ -1,6 +1,6 @@
 export { default as MoveRight } from 'lucide-svelte/icons/move-right';
 export { default as Sun } from 'lucide-svelte/icons/sun';
-export { default as Moon } from 'lucide-svelte/icons/moon';
+export { default as Moon } from 'lucide-svelte/icons/moon-star';
 export { default as Device } from 'lucide-svelte/icons/monitor-smartphone';
 export { default as DropLets } from 'lucide-svelte/icons/droplets';
 export { default as Cookie } from 'lucide-svelte/icons/cookie';
